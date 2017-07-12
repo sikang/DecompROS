1ccf126f96062160d1f7f41fb63f787e48d50973
sikang <lskwdlskwd@gmail.com>
2017-07-12 19:47:32 -0400

    modify README :smirk:

--- test_node/src/test_decomp.cpp
+++ test_node/src/test_decomp.cpp
@@ -2,7 +2,7 @@
 #include "txt_reader.hpp"
 #include <decomp_ros_utils/data_ros_utils.h>
 #include <ros/ros.h>
-#include <decomp_util/ellipse_decomp.h>
+#include <decomp_util/iterative_decomp.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <nav_msgs/Path.h>
 
@@ -42,13 +42,15 @@ int main(int argc, char ** argv){
   if(!read_path(path_file, path))
     ROS_ERROR("Fail to read a path!");
 
+  path = path_downsample(path, 1.0);
+
   nav_msgs::Path path_msg = eigen_to_path(path);
   path_msg.header = header_;
   path_pub.publish(path_msg);
 
-  EllipseDecomp decomp_util(Vec3f(-100, -100, -5), Vec3f(200, 200, 10), true);
+  IterativeDecomp decomp_util(true);
   decomp_util.set_obstacles(obs);
-  decomp_util.decomp(path);
+  decomp_util.decomp_iter(path, 10, true);
 
   decomp_ros_msgs::Ellipsoids es_msg = ellipsoids_to_ros(decomp_util.get_ellipsoids());
   es_msg.header = header_;
