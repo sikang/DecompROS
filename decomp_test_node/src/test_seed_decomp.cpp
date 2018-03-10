a8fa574cc1c987eb9cb06881260ec8bf286669d6
sikang <lskwdlskwd@gmail.com>
2018-03-10 11:53:08 -0500

    update test node

--- decomp_test_node/src/test_seed_decomp.cpp
+++ decomp_test_node/src/test_seed_decomp.cpp
@@ -12,7 +12,7 @@ int main(int argc, char ** argv){
   ros::init(argc, argv, "test");
   ros::NodeHandle nh("~");
 
-  ros::Publisher map_pub = nh.advertise<sensor_msgs::PointCloud2>("cloud", 1, true);
+  ros::Publisher map_pub = nh.advertise<sensor_msgs::PointCloud>("cloud", 1, true);
   ros::Publisher marker_pub = nh.advertise<visualization_msgs::MarkerArray>("markers", 1, true);
   ros::Publisher seed_pub = nh.advertise<sensor_msgs::PointCloud>("seed", 1, true);
   ros::Publisher es_pub = nh.advertise<decomp_ros_msgs::Ellipsoids>("ellipsoids", 1, true);
@@ -26,13 +26,13 @@ int main(int argc, char ** argv){
   nh.param("bag_topic", topic_name, std::string("voxel_map"));
   nh.param("bag_marker", marker_name, std::string("voxel_map"));
   //Read the point cloud from bag
-  sensor_msgs::PointCloud2 map = read_bag<sensor_msgs::PointCloud2>(file_name, topic_name);
-  map.header = header_;
-  map_pub.publish(map);
-
+  sensor_msgs::PointCloud2 cloud2 = read_bag<sensor_msgs::PointCloud2>(file_name, topic_name);
   //Convert into vector of Eigen
   sensor_msgs::PointCloud cloud;
-  sensor_msgs::convertPointCloud2ToPointCloud(map, cloud);
+  sensor_msgs::convertPointCloud2ToPointCloud(cloud2, cloud);
+  cloud.header = header_;
+  map_pub.publish(cloud);
+
   vec_Vec3f obs = DecompROS::cloud_to_vec(cloud);
 
   visualization_msgs::MarkerArray markers = read_bag<visualization_msgs::MarkerArray>(file_name, marker_name);
@@ -56,13 +56,15 @@ int main(int argc, char ** argv){
     //Using seed decomposition
     SeedDecomp decomp_util(it);
     decomp_util.set_obstacles(obs);
-    //decomp_util.dilate(5.0);
+    decomp_util.dilate(5.0);
+    /*
     Aff3f Rf = Trans3f(0, 0, 0) *
       Anglef( 0 * M_PI / 180, Vec3f::UnitX()) *
       Anglef( 10 * M_PI / 180, Vec3f::UnitY()) *
       Anglef(M_PI/3, Vec3f::UnitZ());
 
     decomp_util.dilate(Vec3f(0.5, 0.6, 0.1), Rf.rotation());
+    */
     es.push_back(decomp_util.ellipsoid());
     polys.push_back(decomp_util.polyhedron());
   }
